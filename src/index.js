0a749b9247c1a283d58b596d188828612cf998b1
aiman shakeel <aimanshakeel1008@gmail.com>
2021-02-12 22:20:51 +0530

    initial configuration of server graphQl and Apollo done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,45 @@
-// index.js
-// This is the main entry point of our application
+const express=require("express");
+const {ApolloServer,gql}=require("apollo-server-express");
+
+const port=process.env.PORT || 4000;
+
+
+let notes=[{id:'1',content:'This is a note.',author:'Aiman'},
+            {id:'2',content:'This is also a note.',author:'Aman'},
+            {id:'3',content:'This is a note too.',author:'Amaan'}]
+
+
+const typeDefs=gql `
+    type Note{
+        id:ID!
+        content:String!
+        author:String!
+    }
+    type Query{
+        hello:String!
+        notes:[Note]!
+        note(id:ID!):Note!
+    }
+`;
+
+const resolvers={
+    Query:{
+        hello:()=> `Hello World!!!!!!`,
+        notes:()=>notes,
+        note:(parent,args)=>{
+            return notes.find(note=>note.id===args.id);
+        }
+    }
+}
+
+const app=express();
+
+const server=new ApolloServer({typeDefs,resolvers});
+
+server.applyMiddleware({app,path:'/api'});
+
+app.get('/',(req,res)=>res.send("Hello World!!!!!!!!!!!!!!"));
+
+app.listen(port,()=>{
+    console.log(`server running at https://localhost:${port}${server.graphqlPath}`);
+});
