d098435d93afaa60d2adc770f2d005fca9878909
aiman shakeel <aimanshakeel1008@gmail.com>
2021-02-13 23:29:25 +0530

    added CRUD and custom datetime

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -4,5 +4,34 @@ module.exports={
             content:args.content,
             author:"Aiman Shakeel"
         });
+    },
+    deleteNote:async(parent,args,{models})=>{
+        try{
+        await models.Note.findOneAndRemove({_id:args.id});
+        return true;
+        }
+        catch(err){
+            return false;
+        }
+    },
+    updateNote:async(parent,args,{models})=>{
+        try{
+            return await models.Note.findOneAndUpdate(
+                {
+                    _id:args.id
+                },
+                {
+                    $set:{
+                        content:args.content
+                    }
+                },
+                {
+                    new:true
+                }
+            );
+        }
+        catch(err){
+
+        }
     }
 }
--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,13 @@
 const {gql}=require('apollo-server-express');
 
 module.exports=gql `
+    scalar DateTime
     type Note{
         id:ID!
         content:String!
         author:String!
+        createdAt:DateTime!
+        updatedAt:DateTime!
     }
     type Query{
         notes:[Note]!
@@ -12,5 +15,7 @@ module.exports=gql `
     }
     type Mutation{
         newNote(content:String!):Note!
+        updateNote(id:ID!,content:String!):Note!
+        deleteNote(id:ID!):Boolean!
     }
 `;
